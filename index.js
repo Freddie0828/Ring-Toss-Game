65105604515fe54269fb61502c873dc614ef8816
Freddie0828 <156211947+Freddie0828@users.noreply.github.com>
2024-02-12 22:00:24 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let saveEl1 = document.getElementById("save-el-1")
+let countEl1 = document.getElementById("count-el-1")
+let saveEl2 = document.getElementById("save-el-2")
+let countEl2 = document.getElementById("count-el-2")
+let count1 = 0
+let count2 = 0
+
+function increment1() {
+    count1 += 1
+    countEl1.textContent = count1
+}
+function save1() {
+    let countStr = count1 + " - "
+    saveEl1.textContent += countStr
+    countEl1.textContent = 0
+    count1 = 0
+}
+function increment2() {
+    count2 += 1
+    countEl2.textContent = count2
+}
+function save2() {
+    let countStr = count2 + " - "
+    saveEl2.textContent += countStr
+    countEl2.textContent = 0
+    count2 = 0
+}
